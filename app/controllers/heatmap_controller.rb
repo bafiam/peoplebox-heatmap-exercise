dcac86d8a1fbbc5460e79ebb0a4805ba2f8d7976
bafiam <stevebafiam@gmail.com>
2020-12-11 12:06:42 +0300

    get each department score based on driver process and return json scores

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -9,8 +9,43 @@ class HeatmapController < ApplicationController
     Employee.find_each do |res|
       @dpts.add(res.department)
     end
+    results = []
+    @resp = Hash.new("")
+    @response = Hash.new([])
+    @drivers.each do |drv|
+      Response.find_each do |res|
+        if res.driver_name == drv
+          getEmp = Employee.find(res.employee_id)
+          @dpts.each do |dp|
+            if getEmp.department == dp
+              @response[dp] += [res.score.to_i]
+             
+          end
+          end
+          
+        end
+        
+      
+      end
+    end
+    @drivers.each do |i|
+      hash = {}
+      hash["driver"] = i
+      hash["score"] = {}
+      @response.each do |key, value| 
+       
+        hash["score"][key] = (value.inject(0.0) { |sum, el| sum + el } / value.size).round(3)
+
+      
+      end
+     results << hash
+    end
+    
+    
 
 
-    render json: @dpts
+
+
+    render json: results
   end
 end
